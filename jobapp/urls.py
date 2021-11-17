150214036a95bf9e09a891ac825f39267ef7e743
lucieneferri <78495231+lucieneferri@users.noreply.github.com>
2021-11-17 14:07:25 -0300

    Criação Home page, views e url

--- jobapp/urls.py
+++ jobapp/urls.py
@@ -4,5 +4,6 @@ from core import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.index, name="index"),
+    path('', views.index, name='index'),
+    path('home/', views.home, name='home')
 ]
