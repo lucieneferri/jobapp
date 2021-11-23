a94359113d596634cdb3c74030923039a9435ae1
lucieneferri <78495231+lucieneferri@users.noreply.github.com>
2021-11-23 12:11:18 -0300

    Página de registro de usuário

--- core/views.py
+++ core/views.py
@@ -2,13 +2,16 @@ from django.shortcuts import redirect, render
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
+from django.contrib.auth.forms import UserCreationForm
+
 
 def index(request):
     return render(request, 'index.html')
 
 def loginPage(request):
+    page = 'login'
     if request.method == 'POST':
-        username = request.POST.get('username')
+        username = request.POST.get('username').lower()
         password = request.POST.get('password')
 
         try: #ter certeza que o usuário existe no banco de dados
@@ -26,12 +29,29 @@ def loginPage(request):
         else:
             messages.error(request, 'Username or Password does not exist')
 
-    context={}
+    context={'page': page}
     return render(request, 'login_register.html', context)
 
 def logoutUser(request):
     logout(request)
     return redirect('index')
+
+def registerPage(request):
+    form = UserCreationForm()
+
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)#pega as infos de usuario e senha
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.username = user.username.lower()
+            user.save()
+            login(request, user)
+            return redirect('home')
+        else:
+            messages.error(request, 'An error occour during registration')
+
+
+    return render(request, 'login_register.html', {'form': form})
 
 def home(request):
     return render(request, 'home.html')
--- jobapp/urls.py
+++ jobapp/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('login/', views.loginPage, name='login'),
     path ('logout/', views.logoutUser, name='logout'),
+    path('register/', views.registerPage, name='register'),
     path('home/', views.home, name='home'),   
 ]
